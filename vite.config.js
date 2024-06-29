1531f8270c95dd6da52bd6dc0c523c2a8417f6e5
cerbin1 <gekarez@gmail.com>
2024-06-29 12:38:28 +0200

    Update vite config

--- vite.config.js
+++ vite.config.js
@@ -4,15 +4,4 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 4173,
-    cors: {
-      origin: "*",
-      methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-      allowedHeaders: ["Content-Type", "Authorization"],
-      exposedHeaders: ["Content-Length", "X-Kuma-Revision"],
-      credentials: true,
-      maxAge: 600,
-    },
-  },
 });
